0a611c333124ae54f72eca628eb04af71603cc66
Gabriel Ochsenhofer <gabs@gabs.tv>
2015-08-29 23:26:59 -0300

    fix url scheme proxy pass

--- util/ws.go
+++ util/ws.go
@@ -210,7 +210,11 @@ func (p *ReverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		outreq.Header.Set("X-Forwarded-For", clientIP)
 	}
 	// pass the origin al protocol
-	outreq.Header.Set("X-Forwarded-Proto", req.URL.Scheme)
+	aa := ""
+	if req.TLS != nil { // TODO: fix url scheme
+		aa = "s"
+	}
+	outreq.Header.Set("X-Forwarded-Proto", req.URL.Scheme+aa)
 
 	if useWebsockets {
 		// connect to the proxied server and asks for websockets!
