c79696357f6e926b3bc443a6ac0535366308a532
Gabriel Ochsenhofer <gabs@gabs.tv>
2015-11-11 10:04:43 -0200

    ws url

--- util/ws.go
+++ util/ws.go
@@ -242,6 +242,7 @@ func (p *ReverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 		outreq.Header.Del("Sec-Websocket-Key")
 		outreq.Header.Del("Sec-Websocket-Version")
+		outreq.Header.Del("Sec-Websocket-Extensions")
 
 		proxy2endserver, _, err := websocket.NewClient(c, &url2, outreq.Header, p.WsCFG.ReadBufferSize, p.WsCFG.WriteBufferSize)
 		if err != nil {
