95382386d1333d9cc9fe80ebb92271d70747c285
Gabriel Ochsenhofer <gabriel@pedidopago.com.br>
2019-08-14 16:07:15 -0300

    vbump

--- cmd/sandpiper/main.go
+++ cmd/sandpiper/main.go
@@ -28,7 +28,7 @@ var (
 func main() {
 	stdout = colorable.NewColorableStdout()
 	stderr = colorable.NewColorableStderr()
-	fmt.Fprintln(stdout, ansi.Color("\nSANDPIPER 1.3.0\n", "green"))
+	fmt.Fprintln(stdout, ansi.Color("\nSANDPIPER 1.4.0\n", "green"))
 	// init flags
 	flag.Parse()
 	// print help if needed
