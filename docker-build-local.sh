1e06714cd16b94ef9bdd4ca45bd83e60e916e6d8
Gabriel Ochsenhofer <gabs@gabs.tv>
2021-09-29 18:25:34 -0300

    change to latest

--- docker-build-local.sh
+++ docker-build-local.sh
@@ -2,5 +2,5 @@
 
 GOOS=linux GOARCH=amd64 go build cmd/sandpiper/main.go
 
-docker build -f local.Dockerfile --no-cache -t $DOCKER_ID_USER/sandpiper:apiroutingexp .
-docker push $DOCKER_ID_USER/sandpiper:apiroutingexp
+docker build -f local.Dockerfile --no-cache -t $DOCKER_ID_USER/sandpiper:latest .
+docker push $DOCKER_ID_USER/sandpiper:latest
